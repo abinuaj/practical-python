cf122441ea6db42d9e5fa2f3d7cabb08aa413151
Abinu Ajithan Jyothini <aajyothini@mun.ca>
2020-07-16 09:25:44 -0230

    modified bounce.py pcost.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -6,4 +6,4 @@ height = 100
 while bounce  <=10:
     height = height * (3/5)
     print(bounce,round( height, 4))
-    bounce += 1
+    bounce = bounce + 1
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,8 +1,3 @@
-# pcost.py
-#
-# Exercise 1.27
-def portfolio_cost(filename):
-    '''
 with open('Data/portfolio.csv') as file:
     headers = next(file)
     total = 0
@@ -11,7 +6,4 @@ with open('Data/portfolio.csv') as file:
         no_of_shares = int(row[1])
         price = float(row[2])
         total += no_of_shares * price
-print('total_cost_of_shares', total)
-    '''
-cost = portfolio_cost('Data/portfolio.csv')
-print('Total cost:', cost)
+print('total_price',total)
