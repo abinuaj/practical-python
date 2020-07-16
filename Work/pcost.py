accba5e2ed42b752cd16558e26ca742d49aa8973
abinuaj <abinuaj@gmail.com>
2020-07-16 01:03:50 -0400

    Assignment 4

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+    '''
+with open('Data/portfolio.csv') as file:
+    headers = next(file)
+    total = 0
+    for line in file:
+        row = line.split(',')
+        no_of_shares = int(row[1])
+        price = float(row[2])
+        total += no_of_shares * price
+print('total_cost_of_shares', total)
+    '''
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
