06f1bc738203e9cd884a01800cf75bec3411a83a
Abinu Ajithan Jyothini <aajyothini@mun.ca>
2020-07-20 20:28:20 -0230

    modified pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,9 +1,24 @@
-with open('Data/portfolio.csv') as file:
-    headers = next(file)
-    total = 0
-    for line in file:
-        row = line.split(',')
-        no_of_shares = int(row[1])
-        price = float(row[2])
-        total += no_of_shares * price
-print('total_price',total)
+import csv
+def portfolio_cost(filename):
+    total_cost = 0.0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost += nshares * price
+            except ValueError:
+                print('Bad row:', row)
+
+    return total_cost
+
+import sys
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
