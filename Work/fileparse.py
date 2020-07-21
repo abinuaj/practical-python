820dcea32ab05ffa56621e505c75c5d68c9aef81
Abinu Ajithan Jyothini <aajyothini@mun.ca>
2020-07-21 20:52:30 -0230

    exercise 3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,12 +3,14 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select = None, types =None, has_headers=True):
+def parse_csv(filename, select = None, types =None, has_headers=True, delimiter=','):
   
     with open(filename) as f:
-        rows = csv.reader(f)
-
-
+        #if delimiter:
+        #    rows = csv.reader(f, delimiter= delimiter)
+        #else:
+            #rows = csv.reader(f)
+        rows = csv.reader(f, delimiter= delimiter)
         headers = next(rows)
         if select:
             indices = [headers.index(colname) for colname in select]
@@ -37,3 +39,7 @@ records = parse_csv('Data/portfolio.csv', types=[str, int, float], select = ['na
 print(records)
 records = parse_csv('Data/prices.csv',types =[str, float], has_headers= False)
 print(records)
+records = parse_csv('Data/portfolio.dat',types =[str, int, float],delimiter=' ')
+print(records)
+#records = parse_csv('signif.txt.tsv'), types=[float, str, int], delimiter='\t', select=['EQ_PRIMARY', 'COUNTRY','YEAR'])
+
