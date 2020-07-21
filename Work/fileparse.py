60a91316097efcccfb739093fd0613db5d37186d
Abinu Ajithan Jyothini <aajyothini@mun.ca>
2020-07-21 12:34:51 -0230

    exercise 3.3

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,20 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+def parse_csv(filename):
+  
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        # Read the file headers
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:    
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
